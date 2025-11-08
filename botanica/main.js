69d34f647097c9c6c60b30586d6eb85b24865b54
dima <168024863+krovskiy@users.noreply.github.com>
2025-11-09 01:53:46 +0200

    new: design version 1.0

---
+++ botanica/main.js
@@ -0,0 +1,31 @@
+  document.getElementById("screenshot").addEventListener("click", async () => {
+    const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+    const deviceScaleFactor = 3;
+
+    await chrome.debugger.attach({ tabId: tab.id }, "1.3");
+
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Page.enable");
+
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Emulation.setDeviceMetricsOverride", {
+      width: 4096,
+      height: 2160,
+      deviceScaleFactor,
+      mobile: false,
+      scale: 1
+    });
+
+    await new Promise(resolve => setTimeout(resolve, 500));
+
+    const screenshot = await chrome.debugger.sendCommand({ tabId: tab.id }, "Page.captureScreenshot", {
+      format: "png"
+    });
+    const container = document.getElementById("preview");
+
+    container.src = "data:image/png;base64," + screenshot.data;
+
+
+    await chrome.debugger.sendCommand({ tabId: tab.id }, "Emulation.clearDeviceMetricsOverride");
+    await chrome.debugger.detach({ tabId: tab.id });
+  });
+
